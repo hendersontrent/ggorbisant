07faf2425245c601a80cbc6511000b2dc60b237e
hendersontrent <trent.henderson1@outlook.com>
2020-04-15 10:41:38 +1000

    More function updates

--- R/orbisant.R
+++ R/orbisant.R
@@ -12,7 +12,6 @@
 #' ggplot(df, aes(x = x, y = x)) +
 #'  geom_point() +
 #'  theme_orbisant()
-#' 
 
 theme_orbisant <- function(){
   theme_bw() +  
@@ -33,6 +32,59 @@ theme_orbisant <- function(){
           plot.title = element_text(colour = orbisant_col("dark_blue")),
           plot.subtitle = element_text(colour = orbisant_col("dark_blue")),
           plot.caption = element_text(colour = orbisant_col("dark_blue")))
+}
+
+#' Orbisant colour scale
+#' 
+#' @param discrete Whether the colour aesthetic is discrete or not
+#' @param reverse Whether the colour palette should be reversed
+#' @param ... Additional arguments
+#'
+#' @rdname scale_colour_orbisant
+#' @export
+#' 
+#' @examples
+#' library(ggplot2)
+#' ggplot(df, aes(x = x, y = x, colour = category)) +
+#'  geom_point() +
+#'  scale_colour_orbisant() +
+#'  theme_orbisant()
+#'  
+#' @importFrom ggplot2 discrete_scale scale_color_gradientn
+
+scale_colour_orbisant <- function(discrete = TRUE, reverse = FALSE, ...) {
+  pal <- orbisant_pal(reverse = reverse)
+  
+  if (discrete) {
+    discrete_scale("colour", "orbisant", palette = pal, ...)
+  } else {
+    scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
+#' @importFrom grDevices colorRampPalette
+orbisant_pal <- function(reverse = FALSE, ...) {
+  pal <- orbisant_col()
+  pal <- pal[!(names(pal) %in% c("dark_blue", "tiffany_blue", "white"))]
+  
+  if (reverse) {
+    pal <- rev(pal)
+  }
+  colorRampPalette(pal, ...)
+}
+
+#' @export
+#' @rdname scale_colour_orbisant
+scale_color_orbisant <- scale_colour_orbisant
+
+orbisant_col <- function(...) {
+  cols <- c(...)
+  
+  if (is.null(cols)) {
+    return(orbisant_colours)
+  }
+  
+  orbisant_colours[cols]
 }
 
 #' General colour palette imports as hex codes
